b56019fd7270475c576e814191b180a51deb4ace
john-paul360 <360stpaulj@gmail.com>
2025-05-14 11:42:27 +0100

    added cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors")
 const PORT = process.env.PORT || 3000;
 const userRouter = require("./routes/userRouter");
 
 //middleware
 app.use(express.json());
+app.use(cors());
 
 //routes
 app.get("/", (req, res) => {
